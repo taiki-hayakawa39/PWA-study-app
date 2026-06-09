63d5f1420c4fa9b117ad2b603f307b0c7b294eb6
早川泰生 <c243212@ed.nagoya-cu.ac.jp>
2026-06-09 16:11:59 +0900

    Load theme overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./enhancements.css";
+import "./theme-overrides.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
